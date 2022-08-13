501787212053d2ac5dda44ebf5b506e1a6ca1252
Beksultan <96925396+Vazno@users.noreply.github.com>
2022-08-13 19:02:36 +0600

    Added new tests for themes

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,7 +1,7 @@
 import json
 
 
-def test_json():
+def test_stats():
     from solipsist.utils import resource_path
 
     with open(resource_path("settings.json")) as f:
@@ -10,3 +10,22 @@ def test_json():
     assert (test_j["stats"]["CLICKS"] == 0) is True
     assert (test_j["stats"]["TIME_PLAYED"] == 0) is True
     assert (test_j["stats"]["BEST_RECORD"] == 0) is True
+
+def test_themes():
+    from solipsist.utils import resource_path
+    with open(resource_path("settings.json")) as f:
+        test_j = json.load(f)
+    max_theme_num = test_j["graphic"]["max_theme"]+1 # Non programmic indexing
+    assert len(test_j["graphic"]["FONT_COLOR"]) == max_theme_num
+    assert len(test_j["graphic"]["PLAYER_COLOR"]) == max_theme_num
+    assert len(test_j["graphic"]["BACKGROUND"]) == max_theme_num
+    assert len(test_j["graphic"]["OBSTACLE_COLOR"]) == max_theme_num
+    assert len(test_j["graphic"]["InputBox"]["COLOR_ACTIVE"]) == max_theme_num
+    assert len(test_j["graphic"]["InputBox"]["COLOR_INACTIVE"]) == max_theme_num
+
+def test_music():
+    from solipsist.utils import resource_path
+    with open(resource_path("settings.json")) as f:
+        test_j = json.load(f)
+    assert test_j["music"]["VOLUME"] <= 1
+    assert test_j["music"]["EFFECT_VOLUME"] <= 1
