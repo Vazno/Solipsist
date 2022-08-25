48c8afec77bc57cd7e62a856ee3a021dacbe5472
Beksultan <96925396+Vazno@users.noreply.github.com>
2022-08-25 15:32:04 +0600

    Fixed encoding bug

--- solipsist/GSS.py
+++ solipsist/GSS.py
@@ -44,7 +44,7 @@ except FileNotFoundError:
 
 def get_language():
     languages = os.listdir(resource_path("translations/"))
-    with open(os.path.join(resource_path('translations/'), languages[j['language']]), "r") as f:
+    with open(os.path.join(resource_path('translations/'), languages[j['language']]), "r", encoding="utf-8") as f:
         _ = yaml.safe_load(f)
         return _
 
